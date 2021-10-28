8b47b07ece579624a665ec36b467c51d7dbde68f
jbonanno <jbonanno@slack-corp.com>
2021-10-28 19:11:41 -0400

    created profile pic containers

---
+++ pages/meet/mentees.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from 'react';
+
+interface Props {
+
+}
+
+function Mentees({ }: Props): ReactElement {
+	return (
+		<div>
+			Mentees
+		</div>
+	);
+}
+
+export default Mentees;
---
+++ pages/meet/mentors.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from 'react';
+import Spotlight from '../../components/meet/spotlight/Spotlight';
+
+interface Props {
+
+}
+
+function Mentors({ }: Props): ReactElement {
+	return (
+		<div>
+			<Spotlight />
+		</div>
+	);
+}
+
+export default Mentors;
