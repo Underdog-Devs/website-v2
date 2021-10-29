6a275ada9d16dd80b9cf5f6f192ff7f3780820f6
jbonanno <jbonanno@slack-corp.com>
2021-10-29 10:29:58 -0400

    removed unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,9 @@ import Head from 'next/head';
 import React from 'react';
 import styles from '../styles/Home.module.scss';
 // components
-import Header from './parts/navigation/Navigation';
 import Hero from '../components/landing/hero/hero';
 import Stats from '../components/landing/stats/Stats';
 import Mentor from '../components/landing/mentor/Mentor';
-import Footer from './parts/footer/Footer';
 import Mentee from '../components/landing/mentee/Mentee';
 import SocialMedia from '../components/landing/socialMedia/SocialMedia';
 import GetInvolved from '../components/landing/getInvolved/GetInvolved';
