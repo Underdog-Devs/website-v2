dc5f682029e02da16c3f6deb24fae06c87991423
jbonanno <jbonanno@slack-corp.com>
2021-10-30 19:54:48 -0400

    organized sass global styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,7 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import SiteHead from '../components/siteHead';
-// styles
-import styles from '../styles/Home.module.scss';
+
 // components
 import Hero from '../components/landing/hero/hero';
 import Stats from '../components/landing/stats/Stats';
@@ -15,7 +14,7 @@ import GetInvolved from '../components/landing/getInvolved/GetInvolved';
 const Home: NextPage = () => (
 	<>
 		<SiteHead title="Underdogs Devs" description="We are a group of software engineers supporting formerly incarcerated and disadvantaged aspiring developers" />
-		<div className={styles.container}>
+		<div>
 			<Hero />
 			<Stats />
 			<Mentor />
