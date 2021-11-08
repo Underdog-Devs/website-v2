0bdd644def7a0c833f054dd7725b00f9e5408465
MisterC-Rogers <c.a.r0g3r5@gmail.com>
2021-11-07 17:08:09 -0700

    responsiveNav

--- pages/parts/navigation/Navigation.tsx
+++ pages/parts/navigation/Navigation.tsx
@@ -1,36 +1,80 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
+import { FaBars } from 'react-icons/fa';
 import styles from './navigation.module.scss';
 
 interface Props { }
 
 function Navigation({ }: Props): ReactElement {
+	const [showLinks, setShowLinks] = useState(false);
+	const linksContainerRef = useRef<HTMLElement | null>(null);
+
+	const toggleLinks = () => {
+		setShowLinks(!showLinks);
+	};
+
+	useEffect(() => {
+		if (showLinks) {
+			if (linksContainerRef.current) {
+				linksContainerRef.current.style.height = 'auto';
+			}
+		} else {
+			if (linksContainerRef.current) {
+				linksContainerRef.current.style.height = '0px';
+			}
+		}
+	}, [showLinks]);
+
 	return (
 		<div className={styles.container}>
-			<Link href="/" passHref>
-				<div>
-					<img
-						className={styles.image}
-						src="/images/underdogdevs-03.png"
-						height={175}
-						width={175}
-						alt="Your Name"
-						loading="lazy"
-					/>
+			<div className={styles.desktopNav}>
+				<Link href="/" passHref>
+					<div>
+						<img
+							className={styles.image}
+							src="/images/underdogdevs-01.png"
+							height={175}
+							width={175}
+							alt="Underdog devs"
+						/>
+					</div>
+				</Link>
+				<nav className={styles.navigation}>
+					<div className={styles.navigationLinks}>
+						<a href="https://forms.gle/uS1m9wMeC75reYte7" target="_blank" rel="noreferrer">
+							Become a Mentee
+						</a>
+						<a href="https://forms.gle/UKw3cQKmmmAEZREu6" target="_blank" rel="noreferrer">Become a Mentor</a>
+						{/* <a href="link">Project Underdog</a> */}
+					</div>
+					<a href="mailto:underdogdevsteam@gmail.com" target="_blank" className={styles.getInvolvedButton} rel="noreferrer">
+						Contact Us
+					</a>
+				</nav>
+			</div>
+			<div className={styles.mobileNav}>
+				<div className={styles.navHeader}>
+					<Link href="/" passHref>
+						<img
+							className={styles.image}
+							src="/images/icon-01.png"
+							height={75}
+							width={75}
+							alt="Underdog devs"
+						/>
+					</Link>
+					<button className={styles.navToggle} onClick={toggleLinks}>
+						<FaBars />
+					</button>
 				</div>
-			</Link>
-			<nav className={styles.navigation}>
-				<div className={styles.navigationLinks}>
+				<nav className={styles.mobileNavigation} ref={linksContainerRef}>
 					<a href="https://forms.gle/uS1m9wMeC75reYte7" target="_blank" rel="noreferrer">
 						Become a Mentee
 					</a>
 					<a href="https://forms.gle/UKw3cQKmmmAEZREu6" target="_blank" rel="noreferrer">Become a Mentor</a>
-					{/* <a href="link">Project Underdog</a> */}
-				</div>
-				<a href="mailto:underdogdevsteam@gmail.com" target="_blank" className={styles.getInvolvedButton} rel="noreferrer">
-					Contact Us
-				</a>
-			</nav>
+					<a href="mailto:underdogdevsteam@gmail.com" target="_blank" rel="noreferrer">Contact Us</a>
+				</nav>
+			</div>
 		</div>
 	);
 }
