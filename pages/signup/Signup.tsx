eb04c3b04422628409ebdb806602ccb4347937e5
robertjallen <bobbidigi34@gmail.com>
2021-11-10 19:38:39 -0500

    refactor form to use react-hook-form

--- pages/signup/Signup.tsx
+++ pages/signup/Signup.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 export default function Signup() {
+	const { register, handleSubmit, formState: { errors } } = useForm();
+	const onSubmit = (data) => console.log(data);
+	console.log(errors);
+
 	return (
 		<div>
 			<i>back icon</i>
 			<button> Back </button>
 			<p>Sign Up To Recieve More Information</p>
 			<p>Someone from the intake team will reach out.</p>
-			<form>
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<label htmlFor="">email address</label>
-				<input type="text" placeholder="enter email address" />
-				<button>Send</button>
+				<input type="text" placeholder="enter email address" {...register('Email', { required: true, pattern: /^\S+@\S+$/i })} />
+				<input type="submit" />
 			</form>
 		</div>
 	);
