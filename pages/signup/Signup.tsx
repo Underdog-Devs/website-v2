7ce109a86cb0c503e47a071cd1d86b7ce3b0496e
jbonanno <jbonanno@slack-corp.com>
2021-12-05 11:42:23 -0500

    added quote to signup

--- pages/signup/Signup.tsx
+++ pages/signup/Signup.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import BackButton from '../../components/landing/BackButton/BackButton';
+import Quote from '../../components/quote';
 import styles from './signup.module.scss';
 
 export default function Signup() {
@@ -10,16 +11,19 @@ export default function Signup() {
 
 	return (
 		<div className={styles.container}>
-			<BackButton />
-			<div className={styles.information}>
-				<h3>Sign Up To Recieve More Information</h3>
-				<p>Someone from the intake team will reach out.</p>
+			<Quote />
+			<div className={styles.signupContainer}>
+				<BackButton />
+				<div className={styles.information}>
+					<h3>Sign Up To Recieve More Information</h3>
+					<p>Someone from the intake team will reach out.</p>
+				</div>
+				<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+					<label htmlFor="">Email address</label>
+					<input className={styles.input} type="text" placeholder="Enter email address" {...register('Email', { required: true, pattern: /^\S+@\S+$/i })} />
+					<input className={styles.submit} type="submit" />
+				</form>
 			</div>
-			<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-				<label htmlFor="">Email address</label>
-				<input className={styles.input} type="text" placeholder="Enter email address" {...register('Email', { required: true, pattern: /^\S+@\S+$/i })} />
-				<input className={styles.submit} type="submit" />
-			</form>
 		</div>
 	);
 }
