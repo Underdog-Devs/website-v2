028efc8220e433ef1a099809740c0e4fd9011dba
jbonanno <jbonanno@slack-corp.com>
2021-12-05 13:06:04 -0500

    removed react-hook-form

--- pages/signup/Signup.tsx
+++ pages/signup/Signup.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
 import BackButton from '../../components/landing/BackButton/BackButton';
 import Quote from '../../components/quote';
 import styles from './signup.module.scss';
 
 export default function Signup() {
-	const { register, handleSubmit, formState: { errors } } = useForm();
 	const onSubmit = (data: any) => console.log(data);
-	console.log(errors);
 
 	return (
 		<div className={styles.container}>
@@ -18,10 +15,10 @@ export default function Signup() {
 					<h3>Sign Up To Recieve More Information</h3>
 					<p>Someone from the intake team will reach out.</p>
 				</div>
-				<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+				<form className={styles.form} onSubmit={onSubmit}>
 					<div className={styles.inputContainer}>
 						<label htmlFor="">Email Address</label>
-						<input className={styles.input} type="text" placeholder="Enter email address" {...register('Email', { required: true, pattern: /^\S+@\S+$/i })} />
+						<input className={styles.input} type="text" placeholder="Enter email address" />
 					</div>
 					<input className={styles.submit} type="submit" />
 				</form>
