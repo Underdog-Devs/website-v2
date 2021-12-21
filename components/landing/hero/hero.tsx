8734e8a475f75ae9083c49a597807fa3ba31b04e
jbonanno <jbonanno@slack-corp.com>
2021-12-21 09:54:27 -0500

    fixed typo

--- components/landing/hero/hero.tsx
+++ components/landing/hero/hero.tsx
@@ -25,7 +25,7 @@ const Hero: React.FC<Props> = ({ }) => {
 				<div className={styles.about}>
 					<h2>Underdog <br />Devs</h2>
 					<p className={styles.about}>
-						We are a group of software engineers helping aspiring developers who are either formerly incarcerated or from an economically disadvantaged background. <br /> We are creatting opportunities in tech for people who might not otherwise get an opportunity <br /> We were founded in September of 2020 <br /> We
+						We are a group of software engineers helping aspiring developers who are either formerly incarcerated or from an economically disadvantaged background. <br /> We are creating opportunities in tech for people who might not otherwise get an opportunity <br /> We were founded in September of 2020 <br /> We
 						currently depend on 100% volunteer support
 					</p>
 				</div>
