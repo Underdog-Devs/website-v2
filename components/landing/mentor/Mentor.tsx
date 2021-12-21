aea8a60edbf63bfae3c63583c2a3d8b18e409c46
jbonanno <jbonanno@slack-corp.com>
2021-12-20 19:57:00 -0500

    fixed double paragraph and padding

--- components/landing/mentor/Mentor.tsx
+++ components/landing/mentor/Mentor.tsx
@@ -19,13 +19,6 @@ function Mentor({}: Props): ReactElement {
 					practice. The mentors develop relationships with the potential for referrals as their mentee
 					develops and becomes job ready.
 				</p>
-				<p>
-					Our mentors are industry experts. They are experienced software engineers from all over the industry
-					who are committed to guiding the Underdog Devs. They have bi-weekly meetings with their mentees
-					where they offer support in the form of encouragement, interview prep, resume analysis, and coding
-					practice. The mentors develop relationships with the potential for referrals as their mentee
-					develops and becomes job ready.
-				</p>
 			</div>
 		</section>
 	);
