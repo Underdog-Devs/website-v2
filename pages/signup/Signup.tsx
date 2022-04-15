91a8fb7b8e174b13ca17827cd13ee626e669bb2a
Jess Bonanno <jbonanno@slack-corp.com>
2022-04-14 21:50:35 -0400

    removed unused code

--- pages/signup/Signup.tsx
+++ pages/signup/Signup.tsx
@@ -1,43 +1,9 @@
-import React, { useState } from 'react';
-// import emailjs from 'emailjs-com';
+import React from 'react';
 import BackButton from '../../components/landing/BackButton/BackButton';
 import Quote from '../../components/quote';
 import styles from './signup.module.scss';
 
 export default function Signup() {
-	// const [email, setEmail] = useState('');
-	// eslint-disable-next-line no-unused-vars
-	const [errors, setErrors] = useState('');
-
-	// const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-	// 	const { value } = e.target;
-	// 	setEmail(value);
-	// };
-
-	// const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
-	// 	e.preventDefault();
-	// 	const emailValid = /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i.test(email);
-	// 	if (emailValid) {
-	// 		emailjs
-	// 			.sendForm(
-	// 				process.env.NEXT_PUBLIC_SERVICE_ID || '',
-	// 				process.env.NEXT_PUBLIC_TEMPLATE_ID || '',
-	// 				e.currentTarget,
-	// 				process.env.NEXT_PUBLIC_USER_ID,
-	// 			)
-	// 			.then(
-	// 				(result) => {
-	// 					console.log(result.text);
-	// 				},
-	// 				(error) => {
-	// 					console.log(error.text);
-	// 				},
-	// 			);
-	// 	}
-	// 	setEmail('');
-	// 	setErrors('');
-	// };
-
 	return (
 		<div className={styles.container}>
 			<Quote />
@@ -46,7 +12,6 @@ export default function Signup() {
 				<div className={styles.information}>
 					<h3>Send us an email to receive more information</h3>
 					<p>Someone from the intake team will reach out.</p>
-					{/* <p> - OR - </p> */}
 
 					<p>
 						You can reach the team directly at <br />
@@ -54,19 +19,6 @@ export default function Signup() {
 					</p>
 
 				</div>
-				{/* <form className={styles.form} onSubmit={sendEmail}>
-					<div className={styles.inputContainer}>
-						<label htmlFor="">Email Address</label>
-						<input
-							className={styles.input}
-							type="email"
-							placeholder="Enter email address"
-							value={email}
-							onChange={handleEmailChange}
-						/>
-					</div>
-					<input className={styles.submit} type="submit" />
-				</form> */}
 			</div>
 		</div>
 	);
