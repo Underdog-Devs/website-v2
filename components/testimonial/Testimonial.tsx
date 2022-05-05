74c1943ccfbb4514e714dde9774ba53d39b558bf
Oculareo <oculareo@gmail.com>
2022-05-05 09:31:50 -0400

    Fixed NextJS image links and React syntax

--- components/testimonial/Testimonial.tsx
+++ components/testimonial/Testimonial.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import styles from './Testimonial.module.scss';
-// import quotationMark from '../../public/images/quotation-mark.svg';
 
 function Testimonial() {
 	return (
 		<div className={styles.container}>
-			<div className="header">
-				<div className="quotation">
-					<p>This is where the quotation svg will go</p>
-				</div>
-				<h1>
-					Testimonial
-				</h1>
+			<div className={styles.header}>
+				<img
+					className={styles.quotation_mark}
+					src="/images/quotation-mark.svg"
+					alt="Large quotation mark"
+				/>
+				<h1>Testimonial</h1>
 			</div>
 			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, neque aspernatur. Quis voluptas numquam sequi iste qui hic rerum, quod optio eum at repellendus esse, similique, illum fuga tempore repudiandae.
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, neque
+				aspernatur. Quis voluptas numquam sequi iste qui hic rerum, quod optio
+				eum at repellendus esse, similique, illum fuga tempore repudiandae.
 			</p>
-			<div className="author">
-				{/* <img></img> */}
-				<div className="authorInfo">
+			<div className={styles.author}>
+				<img
+					className={styles.authorImage}
+					src="/images/mentee-profile 1.png"
+					alt="Profile of mentee"
+				/>
+				<div className={styles.authorInfo}>
 					<p>name</p>
 					<p>title</p>
 				</div>
