5d1cd4bdf53d99f7bc302b1274074e443ac18b95
JimWasHere <98727234+JimWasHere@users.noreply.github.com>
2022-05-08 16:50:35 -0500

    Designed card for featured component

--- components/blog/index.ts
+++ components/blog/index.ts
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export * from './BlogPreviewCard';
+=======
+export * from './Featured';
+>>>>>>> 662007f (Designed card for featured component)
---
+++ pages/test.tsx
@@ -0,0 +1,18 @@
+import { Featured } from '../components/blog/Featured';
+
+const Test = () => {
+	return (
+		<div>
+			<Featured
+				img="./images/collab.jpg"
+				title="Lorem ipsum dolor sit amet "
+				text="Lorem ipsum dolor sit amet consectetur adipisicing elit. In praesentium impedit illum eaque omnis laudantium nihil sunt quis incidunt neque.
+"
+				author="Johanna Murray"
+				date="02 May"
+			/>
+		</div>
+	);
+};
+
+export default Test;
