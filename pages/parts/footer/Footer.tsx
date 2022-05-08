2947c7ade615fadae9bcd80913785ea11c4052bc
Jess Bonanno <jessbonanno@gmail.com>
2022-05-08 10:46:11 -0400

    Development (#20)
    * update packages
    
    * remove mentor file
    
    * minor footer changes
    
    * fixed instagram icon added right year & added links
    
    * lint
    
    * added links to footer & fix spacing
    
    * fixed repsonsive footer styles
    
    * tsxThis is the Login page progress made by William and Caleb on Thursday April 28th
    
    * fixed the error point out after running npm run lint:fix
    
    * deleted and reinstalled node modules
    
    * added new login styles
    
    * updated linkedin urls
    
    * Created the blog preview cards
    
    * added import rule to ES lint
    
    * Updated styles for card
    
    * Updated file names
    
    * moved login page to components so its not live when merged to main
    
    * updated packages
    
    * upgraded to next 12
    
    Co-authored-by: Jess Bonanno <jbonanno@slack-corp.com>
    Co-authored-by: dandavisjs <dandavisjs@protonmail.com>
    Co-authored-by: Caleb Vance <vancecaleb5@gmail.com>
    Co-authored-by: UnderdogDevsIntake <103703028+UnderdogDevsIntake@users.noreply.github.com>
    Co-authored-by: Brandon <Grant.b2134@gmail.com>

--- pages/parts/footer/Footer.tsx
+++ pages/parts/footer/Footer.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link';
 import React, { ReactElement } from 'react';
-import { BsTwitter, BsFacebook, BsInstagram, BsYoutube, BsGithub, BsLinkedin } from 'react-icons/bs';
+import { BsTwitter, BsFacebook, BsYoutube, BsGithub, BsLinkedin } from 'react-icons/bs';
+import { ImInstagram } from 'react-icons/im';
 // styles
 import styles from './footer.module.scss';
 
-interface Props {}
+interface Props { }
 
-function Footer({}: Props): ReactElement {
+function Footer({ }: Props): ReactElement {
 	return (
 		<div className={styles.container}>
 			<div className={styles.content}>
@@ -43,7 +45,7 @@ function Footer({}: Props): ReactElement {
 							href="https://www.instagram.com/underdog_devs/"
 							className={styles.icon}
 						>
-							<BsInstagram />
+							<ImInstagram />
 						</a>
 						<a
 							target="_blank"
@@ -73,23 +75,34 @@ function Footer({}: Props): ReactElement {
 				</div>
 				<div className={styles.navigation}>
 					<div className={styles.navSect}>
-						<h4>Mentees</h4>
+						<h4>Community</h4>
 						<a href="https://forms.gle/YdE9SBfJGXc3XW928" target="_blank" rel="noreferrer">Become a Mentee</a>
-					</div>
-					<div className={styles.navSect}>
-						<h4>Mentors</h4>
 						<a href="https://forms.gle/qsusfwyTA8H2vj6z5" target="_blank" rel="noreferrer">Become a Mentor</a>
 					</div>
-					{/* <div className={styles.navSect}>
+					<div className={styles.navSect}>
 						<h4>Organization</h4>
-						<a href="link">About</a>
-						<a href="link">Terms</a>
-						<a href="link">Get Involved</a>
-						<a href="link">Privacy Policy</a>
-					</div> */}
+						<Link
+							href="/project-underdog"
+							passHref
+						>
+							Project Underdog
+						</Link>
+						<Link
+							href="/donate"
+							passHref
+						>
+							Donate
+						</Link>
+						<Link
+							href="/signup"
+							passHref
+						>
+							Contact Us
+						</Link>
+					</div>
 				</div>
 			</div>
-			<p className={styles.copyright}>Copyright © 2021. Underdog Devs</p>
+			<p className={styles.copyright}>Copyright © {new Date().getFullYear()} Underdog Devs</p>
 		</div>
 	);
 }
