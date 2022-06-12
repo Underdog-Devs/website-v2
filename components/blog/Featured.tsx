0662e63b8a02a846a9ed87472630155784850080
Jim Farris <98727234+JimWasHere@users.noreply.github.com>
2022-06-12 15:37:56 -0500

    Blog featured (#21)
    * Designed card for featured component
    
    * installed packages
    
    * add trailing line
    
    * add padding to preview card
    
    * minor css adjustments
    
    * deleted test page
    
    Co-authored-by: Jess Bonanno <jbonanno@slack-corp.com>

---
+++ components/blog/Featured.tsx
@@ -0,0 +1,35 @@
+import styles from './featured.module.scss';
+
+type Props = {
+	img: string;
+	title: string;
+	text: string;
+	author: string;
+	date: string;
+};
+export function Featured(props: Props) {
+	const { img, title, text, author, date } = props;
+	return (
+		<div className={styles.container}>
+			<div className={styles.post}>
+				<h3>{title}</h3>
+				<p>{text}</p>
+				<div className={styles.info}>
+					<span className={styles.readMoreLink}>Read More...</span>
+					<div className={styles.right}>
+						<span className={styles.authorName}>{author}</span>
+						<span className={styles.published}>{date}</span>
+					</div>
+				</div>
+			</div>
+			<img
+				className={styles.postImage}
+				src={img}
+				alt="Card"
+				height={400}
+				width={600}
+				loading="lazy"
+			/>
+		</div>
+	);
+}
