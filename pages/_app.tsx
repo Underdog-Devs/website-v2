7472ca3d58fd0afef045697cfdf881da946d9f04
Al <dandavisjs@protonmail.com>
2023-01-31 12:25:09 -0800

    Added Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,29 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import Layout from '../components/Layout';
+import * as ga from '../lib/ga';
 
 // styles
 import '../styles/index.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleRouteChange = (url:string) => {
+			ga.pageview(url);
+		};
+		// When the component is mounted, subscribe to router changes
+		// and log those page views
+		router.events.on('routeChangeComplete', handleRouteChange);
+
+		// If the component is unmounted, unsubscribe
+		// from the event with the `off` method
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
 	return (
 		<Layout>
 			<Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -43,6 +43,23 @@ class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500;600;700&display=swap"
 						rel="stylesheet"
 					/>
+					{/* Global Site Tag (gtag.js) - Google Analytics */}
+					<script
+						async
+						src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+					/>
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+						}}
+					/>
 				</Head>
 				<body>
 					<Main />
