82d638810ec3bcf38c48b7e0cce2b74ad0a964f6
alur2191 <dandavisjs@protonmail.com>
2023-04-01 15:30:13 -0700

    Added env variables for Amplify

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@
 module.exports = {
 	reactStrictMode: true,
 	output: 'standalone',
+	env: {
+		NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
+		DATABASE_URL: process.env.DATABASE_URL,
+	},
 	images: {
 		unoptimized: true,
 	},
