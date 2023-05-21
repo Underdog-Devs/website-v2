7945db6db61c9aa5284516ad4f96225840388db2
alur2191 <dandavisjs@protonmail.com>
2023-05-21 10:11:50 -0700

    Added a dot

--- components/landing/hero/hero.tsx
+++ components/landing/hero/hero.tsx
@@ -28,7 +28,7 @@ const Hero: React.FC<Props> = ({ }) => {
 						<span>We are a group of software engineers helping aspiring developers who are either formerly incarcerated or from an economically disadvantaged background.</span>
 						<span>We are creating opportunities in tech for people who might not otherwise get an opportunity.</span>
 						<span>We were founded in September of 2020.</span>
-						<span>We currently depend on 100% volunteer support</span>
+						<span>We currently depend on 100% volunteer support.</span>
 					</p>
 				</div>
 			</div>
